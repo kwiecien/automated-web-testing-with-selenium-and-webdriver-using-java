7ec4a449b67def5320fdf86e32aa41469032a48b
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-26 23:08:53 +0200

    webdriver: add ChromeDriver

--- src/main/java/com/kk/WebDriverDemo.java
+++ src/main/java/com/kk/WebDriverDemo.java
@@ -1,12 +1,12 @@
 package com.kk;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverDemo {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
     }
 }
