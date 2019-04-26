d92a740aee3320d5e68a6542875738835cc19d69
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-27 00:25:15 +0200

    webdriver: click the image

--- src/main/java/com/kk/DuckDuckGo.java
+++ src/main/java/com/kk/DuckDuckGo.java
@@ -3,10 +3,13 @@ package com.kk;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 class DuckDuckGo {
     private final WebDriver driver;
     private static final String address = "https://duckduckgo.com/";
+    private static final String imageSelector = "div.tile--img__media";
 
     DuckDuckGo(WebDriver webDriver) {
         driver = webDriver;
@@ -14,10 +17,20 @@ class DuckDuckGo {
 
     void test() {
         driver.get(address);
+        WebDriverWait wait = new WebDriverWait(driver, WebDriverWait.DEFAULT_SLEEP_TIMEOUT);
+
         WebElement searchField = driver.findElement(By.id("search_form_input_homepage"));
         searchField.sendKeys("selenium");
         searchField.submit();
-        WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
+
+        WebElement imagesLink = driver.findElement(By.linkText("Images"));
         imagesLink.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(imageSelector)));
+        WebElement imageElement = driver.findElements(By.cssSelector(imageSelector)).get(0);
+        imageElement.click();
+
+        WebElement imageLink = driver.findElements(By.className("detail__media")).get(0).findElement(By.tagName("a"));
+        imageLink.click();
     }
 }
