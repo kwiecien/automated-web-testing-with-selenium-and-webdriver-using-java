9de73773d160f78f704693c508ae9cc56bdd18e3
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-27 20:41:26 +0200

    webdriver: select a checkbox

---
+++ src/main/java/com/kk/WebDriverCheckboxes.java
@@ -0,0 +1,16 @@
+package com.kk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverCheckboxes {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///C:/Users/kk/dev/automated-web-testing-with-selenium-and-webdriver-using-java/src/main/webapp/CheckboxTest.html");
+        WebElement checkbox = driver.findElement(By.id("lettuceCheckbox"));
+        checkbox.click();
+        checkbox.click(); // undo
+    }
+}
