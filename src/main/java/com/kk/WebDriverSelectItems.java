abfb873c2ad32201ccda132b2184d3ad1c7ae99a
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-27 20:46:43 +0200

    webdriver: use Select

---
+++ src/main/java/com/kk/WebDriverSelectItems.java
@@ -0,0 +1,18 @@
+package com.kk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class WebDriverSelectItems {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///C:/Users/kk/dev/automated-web-testing-with-selenium-and-webdriver-using-java/src/main/webapp/SelectItemTest.html");
+
+        WebElement selectElement = driver.findElement(By.id("select1"));
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("Maybe");
+    }
+}
