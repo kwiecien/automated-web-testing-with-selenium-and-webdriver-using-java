21d41b4bc1c8ca529873c4e6d5334f140be8aa65
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-27 21:01:41 +0200

    webdriver: get table data wit xpath

--- src/main/java/com/kk/WebDriverTables.java
+++ src/main/java/com/kk/WebDriverTables.java
@@ -10,9 +10,11 @@ public class WebDriverTables {
         WebDriver driver = new ChromeDriver();
         driver.get("file:///C:/Users/kk/dev/automated-web-testing-with-selenium-and-webdriver-using-java/src/main/webapp/TableTest.html");
 
-        WebElement outerTable = driver.findElement(By.tagName("table"));
-        WebElement innerTable = outerTable.findElement(By.tagName("table"));
-        WebElement row = innerTable.findElements(By.tagName("td")).get(1);
+        // WebElement outerTable = driver.findElement(By.tagName("table"));
+        // WebElement innerTable = outerTable.findElement(By.tagName("table"));
+        // WebElement row = innerTable.findElements(By.tagName("td")).get(1);
+
+        WebElement row = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[2]/table/tbody/tr[2]/td"));
         System.out.println(row.getText());
     }
 }
